fb717a6759ba40a37977aacc9c4ec8b4b2f14de1
jungsic1023 <jungsic1023@naver.com>
2022-08-28 17:55:00 +0900

    스크립트 정리

--- Assets/Script/Player_script/Player.cs
+++ Assets/Script/Player_script/Player.cs
@@ -11,14 +11,11 @@ public class Player : MonoBehaviour
     public VariableJoystick joy;
     public float speed;
     public Button attack_button;
-    //public GameObject Attack_Ray;
-   // public Button attack_and_shield_button;
-   // public Button on_jump;
-   // public Button on_jump_attack;
+
     Rigidbody rigid;
     Animator anim;
-    public bool is_attack = false;
-    Ray ray;
+
+
     Vector3 moveVec;
 
     private void Awake()
@@ -34,19 +31,13 @@ public class Player : MonoBehaviour
 
         move();
         attack_button.onClick.AddListener(() => attack_animator(true));
-     /*   attack_and_shield_button.onClick.AddListener(() => attack_and_shield_animator(true));
-        on_jump.onClick.AddListener(() => jump_animator(true));
-        on_jump_attack.onClick.AddListener(() => jump_attack_animator(true));*/
+     
 
     }
 
 
 
-    private void LateUpdate()
-    {
-        anim.SetFloat("isWalking", moveVec.sqrMagnitude);
-
-    }
+  
 
     void move()
     {
@@ -64,46 +55,17 @@ public class Player : MonoBehaviour
         Quaternion dirQuat = Quaternion.LookRotation(moveVec);
         Quaternion moveQuat = Quaternion.Slerp(rigid.rotation, dirQuat, 0.3f);
         rigid.MoveRotation(moveQuat); //캐릭터의 보는 방향 제어
+        anim.SetFloat("isWalking", moveVec.sqrMagnitude);
 
     }
     void attack_animator(bool attack_bool)
     {
         if (attack_bool)
-        {
-            is_attack = true;
-            Debug.Log(attack_bool);
-            
+        {  
             anim.SetTrigger("isattack01");
-            //Attack_Ray.GetComponent<RayCast>().attack_raycast();
+          
         }
 
     }
     
-    void attack_and_shield_animator(bool attack_and_shield)
-    {
-        if (attack_and_shield)
-        {
-
-            anim.SetTrigger("isattack02");
-        }
-
-    }
-    void jump_animator(bool on_jump)
-    {
-        if (on_jump)
-        {
-
-            anim.SetTrigger("OnJump");
-        }
-
-    }
-    void jump_attack_animator(bool jump_attack)
-    {
-        if (jump_attack)
-        {
-
-            anim.SetTrigger("jump_attack");
-        }
-
-    }
 }
